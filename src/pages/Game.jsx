2dd413ceafbe17c765d41c7d6d7958183806c6a9
flethcer272 <helixxroc@yandex.ru>
2023-03-11 06:51:04 +0500

    Add Game Page

---
+++ src/pages/Game.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Game = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <h1 className="text-white text-center mt-5">This is Game Page</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
